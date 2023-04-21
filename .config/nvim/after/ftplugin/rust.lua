df17e48ac3853d108dd99df7d948efc68f355a73
Matthew McElroy <mdmcelroy01@gmail.com>
2023-04-22 05:46:32 +1000

    Finish rust debugging config.

--- .config/nvim/after/ftplugin/rust.lua
+++ .config/nvim/after/ftplugin/rust.lua
@@ -1,10 +1,13 @@
 vim.cmd[[packadd termdebug]]
 vim.g.termdebugger = "rust-gdb"
+vim.g.termdebug_useFloatingHover = 0
 
 local split_path = vim.fn.split(vim.fn.getcwd(), '/')
 local project_path = split_path[#split_path]
 local bin_path = 'target/debug/' .. project_path
 
+vim.keymap.set("n", "<leader>D", "<CMD>Termdebug " .. bin_path .. "<CR>")
+vim.keymap.set("n", "<leader>r", "<CMD>Run<CR>")
 vim.keymap.set("n", ";", "<CMD>Step<CR>")
 vim.keymap.set("n", "<leader>;", "<CMD>Over<CR>")
 vim.keymap.set("n", "<leader>b", "<CMD>Break<CR>")
